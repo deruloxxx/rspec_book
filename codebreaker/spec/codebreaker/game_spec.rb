16dc4ac5634f76e4113a24fd35ae03b4c06e572e
ryuyauetake <ryuya@anymindgroup.com>
2022-09-20 00:50:00 +0900

    test: 6.2

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do
+    let(:output) { double('output').as_null_object }
+    let(:game) { Game.new(output) }
+
     describe '#start' do
-      let(:output) { double('output').as_null_object }
-      let(:game) { Game.new(output) }
-      
       it "sends a welcome message" do
         expect(output).to receive(:puts).with('Welcome to Codebreaker!')
-        game.start
+        game.start('1234')
       end
       
       it "prompts for the first guess" do
         expect(output).to receive(:puts).with('Enter guess:')
-        game.start
+        game.start('1234')
       end
     end
   end
